f910f773770549d8a238f42783be577dfd0ace8d
Johan Uribe <49119190+johanuribe03@users.noreply.github.com>
2019-04-30 20:51:05 -0400

    Add files via upload

---
+++ js/myscripts.js
@@ -0,0 +1,23 @@
+
+
+
+
+/*0*/
+var name0 = items[0].name;
+document.getElementById("name0").innerHTML = name0;
+var price0 = items[0].price;
+document.getElementById("price0").innerHTML = price0;
+var shortDescription0 = items[0].shortDescription;
+document.getElementById("shortDescription0").innerHTML = "<b>Short description: </b>" + shortDescription0;
+var rating0 = items[0].rating;
+document.getElementById("rating0").innerHTML = "<b>Rating: </b>" + rating0;
+var ratingCount0 = items[0].ratingCount;
+document.getElementById("ratingCount0").innerHTML = "<b>Rating Count: </b>" + ratingCount0;
+var salePrice0 = items[0].salePrice;
+document.getElementById("salePrice0").innerHTML = "<b>Sale Price: </b>" + salePrice0;
+var brand0 = items[0].brand;
+document.getElementById("brand0").innerHTML = "<b>Brand: </b>" + brand0;
+var prodId0 = items[0].prodId;
+document.getElementById("prodId0").innerHTML = "<b>Product ID: </b>" + prodId0;
+var description0 = items[0].description;
+document.getElementById("description0").innerHTML = "<b>Full Description: </b>" + description0;
